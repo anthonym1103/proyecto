ba652e88dd3e6cdf0c4a1374e35d1d2f821d7fe6
anthonym1103 <lozanoppanthony@gmail.com>
2025-07-18 00:28:46 -0400

    Initial commit

--- main.py
+++ main.py
@@ -1,34 +1,34 @@
 from fastapi import FastAPI, HTTPException
-from src.conexion_postgresql import getPostulacion, getOfertasLaborales, setUserEmpresa, updateOferta
+from src.conexion_postgresql import getPostulacion, getOfertasLaborales, setUserEmpresa, updateOferta, setOfertLaboral
 from modelos.models import Postulacion, OfertaLaboral,UsuarioEmpresa
 
 app = FastAPI()
 ##################ACCIONES DEL USUARIO DE HIRING GROUP###########################
-@app.get("/postulaciones", response_model = list[Postulacion])
+@app.get("/userHG/postulaciones", response_model = list[Postulacion])
 async def postulacion():
     return getPostulacion()
 
-@app.get("/ofertaslaborales", response_model= list[OfertaLaboral])
+@app.get("/userHG/ofertaslaborales", response_model= list[OfertaLaboral])
 async def ofertLaborals():
     return getOfertasLaborales(1)
 
-@app.get("/ofertaslaborales/{area}", response_model = list[OfertaLaboral])
+@app.get("/userHG/ofertaslaborales/{area}", response_model = list[OfertaLaboral])
 async def ofertLaborals(area: str):
     response = getOfertasLaborales(2, area)
     if (len(response) == 0):
         raise HTTPException(status_code = 202, detail = "La oferta laboral no pudo ser encontrada")
     return response
 
-@app.post("/userempresa", response_model = UsuarioEmpresa)
+@app.post("/userHG/createuserempresa", response_model = UsuarioEmpresa)
 async def createUserEmpresa(dataEmpresa: UsuarioEmpresa):
     newEmpresa = dataEmpresa.model_dump()
     if(setUserEmpresa(newEmpresa["nombre"], newEmpresa["rif"], newEmpresa["tlf"], newEmpresa["sector"], newEmpresa["personacontacto"], newEmpresa["idsucursal"])):
         raise HTTPException(status_code = 404, detail = "La empresa ingresada ya existe")
     return newEmpresa
 
-@app.put("/updateOferta/{id}", response_model = OfertaLaboral)
+@app.put("/userHG/updateOferta/{id}", response_model = OfertaLaboral)
 async def actualzOferta(id: int):
-    response = updateOferta(id)
+    response = updateOferta(1, id)
     if(response is False):
         raise HTTPException(status_code = 404, detail = "La oferta a actualizar no existe")
     return response
@@ -36,6 +36,30 @@ async def actualzOferta(id: int):
 
 
 ##################ACCIONES DE EMPRESA###########################
+
+@app.get("/userEmpresa/ofertaslaborales", response_model = list[OfertaLaboral])
+async def ofertsLaborales():
+    return(getOfertasLaborales(1))
+
+@app.post("/userEmpresa/createOfertaLaboral", response_model = OfertaLaboral)
+async def createOfertLaboral(dataOferta: OfertaLaboral):
+    newOfertaLaboral = dataOferta.model_dump()
+    if(setOfertLaboral(newOfertaLaboral["sucursal"], newOfertaLaboral["profesion"], newOfertaLaboral["cargo_vacante"], newOfertaLaboral["descripcion_cargo"], newOfertaLaboral["salario"])):
+        raise HTTPException(status_code = 404, detail = "La empresa ingresada ya existe")
+    return newOfertaLaboral
+
+@app.put("/userEmpresa/updateOferta/{id}", response_model = OfertaLaboral)
+async def actualzOferta(id: int, dataOferta:OfertaLaboral):
+    newOferta = dataOferta.model_dump()
+    for campo in newOferta:
+        if(newOferta[campo] != ""):
+            if((type(newOferta[campo]) is int and newOferta[campo] != 0) or type(newOferta[campo]) is bool):
+                response = updateOferta(2, id, campo, newOferta[campo])
+                if(response is False):
+                    raise HTTPException(status_code = 404, detail = "La oferta a actualizar no existe")
+    return newOferta
+
+
 
 
 ##################ACCIONES DE EMPRESA###########################
--- src/conexion_postgresql.py
+++ src/conexion_postgresql.py
@@ -42,12 +42,27 @@ def insertarDataPostulacion(oferta:int, salario:float, tipoSangre:str, personaCo
     cursor.execute(query)
     cursor.close()
 
-def searchOfertaLaboral(idOferta: int):
+def searchOfertaLaboral(case: int, valor: str):
     cursor = connection.cursor()
     cursor.execute(f"SELECT * FROM ofertalaboral")
     rows = cursor.fetchall()
+    if(case == 1):
+        idOferta = int(valor)  
+        for row in rows:
+            if(row[0]==idOferta):
+                return True
+    elif(case == 2):
+        for row in rows:
+            if(row[3]==valor):
+                return True
+    return False
+
+def isEmpresaInBD(rif: str):
+    cursor = connection.cursor()
+    cursor.execute(f"SELECT * FROM usuarioempresa")
+    rows = cursor.fetchall()
     for row in rows:
-        if(row[0]==idOferta):
+        if(row[2] == rif):
             return True
     return False
 
@@ -91,23 +106,47 @@ def getOfertasLaborales(case: int, area:str=""):
     return listOfertas
 
 def setUserEmpresa(nombre:str, rif:str, telf:str, sector:str, personcontact:str, idsucursal:int):
+    if(isEmpresaInBD(rif) is False):
+      return False 
     cursor = connection.cursor()
     query = f"""INSERT INTO usuarioempresa(nombre, rif, telf, sector, personacontacto, idsucursal) values('{nombre}','{rif}','{telf}','{sector}','{personcontact}',{idsucursal}) """
     cursor.execute(query)
     cursor.close()
 
 
-def updateOferta(idOferta: int):
-    if(searchOfertaLaboral(idOferta) is False):
+def updateOferta(case: int, idOferta: int, campo:str="", valor:str=""):
+    if(searchOfertaLaboral(1,idOferta) is False):
       return False 
     cursor = connection.cursor()
-    query = f"""UPDATE ofertalaboral SET status = false WHERE id = {idOferta}"""
+    if(case == 1):
+        query = f"""UPDATE ofertalaboral SET status = false WHERE id = {idOferta}"""
+    elif(case == 2):
+        if(campo == "idsucursal" or campo == "status" or campo == "salario"):
+            valorFinal = 0
+            if(campo == "salario"):
+                valorFinal = float(valor)
+            valorFinal = int(valor)
+            query = f"""UPDATE ofertalaboral SET {campo} = {valorFinal} WHERE id = {idOferta}"""
+        else:
+            query = f"""UPDATE ofertalaboral SET {campo} = '{valor}' WHERE id = {idOferta}"""
+        cursor.execute(query)
+        cursor.close()
+        return True
+
     cursor.execute(query)
     cursor.close()
     return getOfertasLaborales(3, idOferta)
 ##################ACCIONES DEL USUARIO DE HIRING GROUP###########################
 
 ##################ACCIONES DE EMPRESA###########################
+
+def setOfertLaboral(idSucursal:int, profesion:str, cargoVacante:str, descripcionCargo:str, salario:str):
+    if(searchOfertaLaboral(2,cargoVacante) is False):
+      return False 
+    cursor = connection.cursor()
+    query = f"""INSERT INTO ofertalaboral(idsucursal, profesion, cargo_vacante, descripcion_cargo, salario) VALUES({idSucursal},'{profesion}','{cargoVacante}','{descripcionCargo}',{salario})"""
+    cursor.execute(query)
+    cursor.close()
 
 
 
